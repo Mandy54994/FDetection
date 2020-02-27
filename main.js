16fd76cb7a3b86a0764b33798608a0623e7ae5fa
Mandeep Singh <ms54994@gmail.com>
2020-02-27 19:42:33 +0530

    Landing page non responsive (not completed)

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+function runDot() {
+    console.log('running');
+    setTimeout(function () {
+        $('div span:nth-child(1)').css("background-color", "#6c63ff");
+        $('div span:nth-child(2)').css("background-color", "#e5e5e5");
+        $('div span:nth-child(3)').css("background-color", "#e5e5e5");
+    }, 1000);
+    setTimeout(function () {
+        $('div span:nth-child(2)').css("background-color", "#6c63ff");
+        $('div span:nth-child(1)').css("background-color", "#e5e5e5");
+        $('div span:nth-child(3)').css("background-color", "#e5e5e5");
+    }, 2000);
+    setTimeout(function () {
+        $('div span:nth-child(3)').css("background-color", "#6c63ff");
+        $('div span:nth-child(2)').css("background-color", "#e5e5e5");
+        $('div span:nth-child(1)').css("background-color", "#e5e5e5");
+    }, 3000);
+}
+
+function runDiv() {
+    setTimeout(function () {
+        $('.rectangle10').css("background-color", "#6c63ff");
+        $('.rectangle11').css("background-color", "#e5e5e5");
+        $('.rectangle12').css("background-color", "#e5e5e5");
+    }, 1000);
+    setTimeout(function () {
+        $('.rectangle11').css("background-color", "#6c63ff");
+        $('.rectangle10').css("background-color", "#e5e5e5");
+        $('.rectangle12').css("background-color", "#e5e5e5");
+    }, 2000);
+    setTimeout(function () {
+        $('.rectangle12').css("background-color", "#6c63ff");
+        $('.rectangle11').css("background-color", "#e5e5e5");
+        $('.rectangle10').css("background-color", "#e5e5e5");
+    }, 3000);
+}
 // var slideIndex = 0;
 // showSlides();
 
@@ -23,3 +59,30 @@
 //     var rect11 = document.querySelector('.rectangle11');
 //     var rect12 = document.querySelector('.rectangle12');
 // });
+$(document).ready(function () {
+    console.log("Ready!");
+    runDot();
+    runDiv();
+    var myDOt = setInterval("runDot()", 3000);
+    var myDiv = setInterval("runDiv()", 3000);
+    $('.vom').click(function (e) {
+        e.preventDefault();
+        setTimeout(function () {
+            $('div span:nth-child(1)').css("background-color", "#6c63ff");
+            $('div span:nth-child(2)').css("background-color", "#e5e5e5");
+            $('div span:nth-child(3)').css("background-color", "#e5e5e5");
+        }, 1000);
+        setTimeout(function () {
+            $('div span:nth-child(2)').css("background-color", "#6c63ff");
+            $('div span:nth-child(1)').css("background-color", "#e5e5e5");
+            $('div span:nth-child(3)').css("background-color", "#e5e5e5");
+        }, 2000);
+        setTimeout(function () {
+            $('div span:nth-child(3)').css("background-color", "#6c63ff");
+            $('div span:nth-child(2)').css("background-color", "#e5e5e5");
+            $('div span:nth-child(1)').css("background-color", "#e5e5e5");
+        }, 3000);
+
+    });
+    // $('div span:nth-child(2)').css("background-color", "#6c63ff");
+});
